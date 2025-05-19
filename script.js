be343a72638b1d8d3cc5ff93d3f6640ed1772144
gravesl <graveslol33@gmail.com>
2025-05-19 02:43:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+const boot = document.getElementById("boot");
+const menu = document.getElementById("menu");
+const bootComplete = document.getElementById("boot-complete");
+
+// show "SYSTEM READY" after delay
+setTimeout(() => {
+  bootComplete.style.display = "block";
+}, 2500);
+
+// click to continue
+boot.addEventListener("click", () => {
+  boot.classList.add("hidden");
+  menu.classList.remove("hidden");
+});
